d1a7358ec796d3f4a9ebb2db5cd77f098f8e1070
Ethan <erdesi90562@gmail.com>
2016-01-29 14:19:04 -0500

    create basic game functionality
    You can find a piece and set it's location along with reset the board. (null)

--- assets/scripts/myJavaScript.js
+++ assets/scripts/myJavaScript.js
@@ -1,3 +1,28 @@
 'use strict';
 
+let gameBoard = [
+          [[],[],[]],
+          [[],[],[]],
+          [[],[],[]],
+];
+
+var setSquare = function(player, row, col) {
+  checkerboard[row][col] = player;
+}
+
+var getPieceLocation = function(row, col) {
+  var pieceType = gameBoard[row][col];
+  return pieceType;
+}
+
+function resetBoard() {
+  // Reset the board to null
+  var boardRows = gameBoard.length;
+
+  for (var i = 0; i < boardRows; i++) {
+    for (var z = 0; z < boardRows; z++) {
+      gameBoard[i][z] = null;
+    }
+  }
+}
 module.exports = true;
