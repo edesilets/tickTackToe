d36f723e5506696f6f7bc872ab51a73fd94219ab
Ethan <erdesi90562@gmail.com>
2016-02-01 18:55:17 -0500

    create signUp curl bash

---
+++ api-test/signUp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+EMAIL='test@test.j'
+PASSWORD='anpassword'
+
+BASE_URL="http://tic-tac-toe.wdibos.com"
+URL="${BASE_URL}/sign-up"
+#URL="http://httpbin.org/post"
+
+url() {
+  CONTENT_TYPE="application/x-www-form-urlencoded"
+}
+
+json() {
+  CONTENT_TYPE="application/json"
+
+  curl ${URL} \
+  --include \
+  --request POST \
+  --header "Content-Type: ${CONTENT_TYPE}" \
+  --data "{
+      \"credentials\": {
+        \"email\": \"${EMAIL}\",
+        \"password\": \"${PASSWORD}\"
+    }
+  }"
+
+}
+
+json
+
+echo
+#     "id":6,
+#     "email":"test@test.js"
