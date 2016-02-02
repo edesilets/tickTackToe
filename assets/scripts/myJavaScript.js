54b4be8c04dc054fac26dcaa641be6c77faba01d
Ethan <erdesi90562@gmail.com>
2016-02-02 17:23:33 -0500

    function finds diagonal winner and returns player

--- assets/scripts/myJavaScript.js
+++ assets/scripts/myJavaScript.js
@@ -1,10 +1,35 @@
 'use strict';
 
-const gameBoard = [
-  ['X','O','O'],
-  ['X','X','X'],
-  ['O','O','O']
-];
+const gameBoard = ['x','x','x',3,4,5,6,7,8];
+
+let winnerDIA = function(player, array) {
+  let error = console.log('No Winning Diagnoal found.');
+  let count = 0;
+
+  for (let i = 0; i < array.length; i += 4) {
+    if (array[i] === player) {
+      count++;
+    }
+    if (count === 3) {
+
+      return player;
+    }
+  }
+
+  count = 0;
+
+  for (let i = 2; i < array.length; i += 2) {
+    if (array[i] === player) {
+
+      count++;
+    }
+    if (count === 3) {
+
+      return player;
+    }
+  }
+  return error;
+};
 
 const setSquare = function(player, row, col) {
   gameBoard[row][col] = player;
@@ -25,7 +50,6 @@ const resetBoard = function() {
     }
   }
 }
-debugger;
 
 module.exports = {
   setSquare: setSquare,
