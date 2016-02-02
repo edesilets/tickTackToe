2ef156ebe509ff523b09ccb43abc9c8eab616427
Ethan <erdesi90562@gmail.com>
2016-02-02 17:34:56 -0500

    function to locate a horizontal winner on board
    This needs to me simplified!!!!!

--- assets/scripts/myJavaScript.js
+++ assets/scripts/myJavaScript.js
@@ -2,7 +2,7 @@
 
 const gameBoard = ['x','x','x',3,4,5,6,7,8];
 
-let winnerDIA = function(player, array) {
+let winnerDIA = function(player, array) {                 // See if possible to simplify more!
   let error = console.log('No Winning Diagnoal found.');
   let count = 0;
 
@@ -30,6 +30,36 @@ let winnerDIA = function(player, array) {
   }
   return error;
 };
+
+let winnerHorz = function(player, array) {           // simplify !!!!!
+  let error = console.log('No in line winners.');
+  let count = false;
+  //array inc for expantion use it insed for I so each iteration increases.
+  for (let i = 0; i <= 2; i++) {
+    if (array[i] === player) {
+      count++;
+    }
+    if (count === 3) {
+      return player;
+    }
+  }
+  for (let i = 3; i <= 5; i++) {
+    if (array[i] === player) {
+      count++;
+    }
+    if (count === 3) {
+      return player;
+    }
+  }
+  for (let i = 6; i <= 8; i++) {
+    if (array[i] === player) {
+      count++;
+    }
+    if (count === 3) {
+      return player;
+    }
+  }
+}
 
 const setSquare = function(player, row, col) {
   gameBoard[row][col] = player;
