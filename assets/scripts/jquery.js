3b6c5b3879a5ae0c90cc7f46312d46bf35d09074
Ethan <erdesi90562@gmail.com>
2016-02-04 15:12:23 -0500

    if statement to find out if current spot is empty or occupied

--- assets/scripts/jquery.js
+++ assets/scripts/jquery.js
@@ -7,12 +7,16 @@ let currentPlayer = 'x';
 
 let createMove = function () {
   $('.gameboard').on('click', function () {
-    gameFunctions.gameBoard[event.target.id] = currentPlayer;
-    $(this).text(currentPlayer); // I DONT UNDERSTAND HOW 'this' IS WORKING.
-    count++;
-    //api.updateGame(index , currentplayer);
-    //api push one move at a time
-    currentPlayer = currentPlayer === 'x'  ? 'o' : 'x';
+      if ($(this).text() === '') {
+        gameFunctions.gameBoard[event.target.id] = currentPlayer;
+        $(this).text(currentPlayer); // I DONT UNDERSTAND HOW 'this' IS WORKING.
+        count++;
+        //api.updateGame(index , currentplayer);
+        //api push one move at a time
+        currentPlayer = currentPlayer === 'x'  ? 'o' : 'x';
+      } else {
+        confirm('sorry someone is all ready here!');
+    }
   });
 };
 
