2c500f3ff7e3c8fc0bf72b31a252ae0375649b3a
Ethan <erdesi90562@gmail.com>
2016-02-04 16:52:24 -0500

    create scoreboard object and logic to add wins and losses.

--- assets/scripts/jquery.js
+++ assets/scripts/jquery.js
@@ -4,6 +4,13 @@ let gameFunctions = require('./gameBackend');
 
 let count = 0;
 let currentPlayer = 'x';
+let score = {
+  playerXWins: 0,
+  playerXLoss: 0,
+  playerOWins: 0,
+  playerOLoss: 0,
+  tie : 0
+}
 
 let createMove = function () {
   $('.gameboard').on('click', function () {
@@ -13,6 +20,12 @@ let createMove = function () {
         count++;
         //api.updateGame(index , currentplayer);
         //api push one move at a time
+        if (gameFunctions.checkWinner(currentPlayer)) {
+          confirm(currentPlayer + ' has won!');
+          currentPlayer === 'x' ? score.playerXWins++ : score.playerOWins++;
+          currentPlayer === 'x' ? score.playerOLoss++ : score.playerXLoss++;
+          return;
+        }
         currentPlayer = currentPlayer === 'x'  ? 'o' : 'x';
       } else {
         confirm('sorry someone is all ready here!');
